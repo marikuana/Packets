8ffce000c1101427da7bac4215a61986bc9af663
Marikuana <marians22092000@gmail.com>
2022-10-17 18:25:26 +0300

    PacketFactory add reflection

--- Packets/PacketFactory.cs
+++ Packets/PacketFactory.cs
@@ -1,30 +1,40 @@
 ﻿using System.Reflection;
 using System.IO;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Packets
 {
     public class PacketFactory
     {
+        private Dictionary<PacketType, ConstructorInfo> packets;
+
+        public PacketFactory()
+        {
+            packets = new Dictionary<PacketType, ConstructorInfo>();
+            IEnumerable <Type> packetTypes = Assembly.GetExecutingAssembly().GetTypes().Where(t => t.IsSubclassOf(typeof(Packet)));
+            foreach (Type type in packetTypes)
+            {
+                ConstructorInfo constructor = type.GetConstructors().First();
+                Packet packet = (Packet)constructor.Invoke(new object[0]);
+                packets.Add(packet.packetType, constructor);
+            }
+        }
+
+        private Packet CreatePacket(PacketType packetType)
+        {
+            return (Packet)packets[packetType].Invoke(new object[0]);
+        }
+
         public Packet GetPacket(byte[] buffer)
         {
             var reader = new BinaryReader(new MemoryStream(buffer));
             
             PacketType packetType = (PacketType)reader.ReadUInt16();
 
-            Packet packet;
-            switch (packetType)
-            {
-                case PacketType.Ping:
-                    packet = new Ping();
-                    break;
-                case PacketType.GoTo:
-                    packet = new GoTo();
-                    break;
-                default:
-                    throw new ArgumentException();
-            };
-            
+            Packet packet = CreatePacket(packetType);
+
             packet.Read(reader);
             return packet;
         }
