e469274e36a0f945509d4fa291cb4ba80c57d8ed
Marikuana <marians22092000@gmail.com>
2022-11-04 15:19:03 +0200

    Add ResponcePacketFactory

---
+++ Packets/ResponcePacketFactory.cs
@@ -0,0 +1,24 @@
+﻿namespace Packets
+{
+    public class ResponcePacketFactory
+    {
+        private PacketFactory _packetFactory;
+
+        public ResponcePacketFactory(PacketFactory packetFactory)
+        {
+            _packetFactory = packetFactory;
+        }
+
+        public Response Create()
+        {
+            return new Response(_packetFactory);
+        }
+
+        public Response CreateFromRequest(Request request)
+        {
+            Response response = Create();
+            response.Id = request.Id;
+            return response;
+        }
+    }
+}
