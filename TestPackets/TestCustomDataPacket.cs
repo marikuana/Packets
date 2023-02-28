9d49cbe26fe4ef14c6faa457791d9a7abc13d74e
Marikuana <marians22092000@gmail.com>
2023-02-28 14:57:58 +0200

    Test CustomDataPacket

---
+++ TestPackets/TestCustomDataPacket.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using Packets;
+using System;
+
+namespace TestPackets
+{
+    public class TestCustomDataPacket
+    {
+        public PacketFactory PacketFactory { get; set; }
+        public IServiceProvider ServiceProvider { get; set; }
+        [SetUp]
+        public void Setup()
+        {
+            ServiceProvider = new ServiceProvider();
+            PacketFactory = (PacketFactory)ServiceProvider.GetService(typeof(PacketFactory));
+        }
+
+        [Test]
+        public void Test()
+        {
+            CustomDataPacket customDataPacket = new CustomDataPacket()
+            {
+                CustomData = new CustomData<object>()
+                {
+                    Type = TypeCode.String,
+                    Data = "123sff3t&*gO908r(Ь◙ф└ф.xчоIrФ♥"
+                } 
+            };
+
+            byte[] data = customDataPacket.GetBytes();
+            Packet packet = PacketFactory.GetPacket(data);
+
+            Assert.IsInstanceOf<CustomDataPacket>(packet);
+            CustomDataPacket newPacket = (CustomDataPacket)packet;
+            Assert.AreEqual(customDataPacket.CustomData.Type, newPacket.CustomData.Type);
+            Assert.AreEqual(customDataPacket.CustomData.Data, newPacket.CustomData.Data);
+        }
+    }
+}
